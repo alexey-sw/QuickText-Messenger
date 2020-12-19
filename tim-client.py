cd5360afd87ca6861e979e6aa9255d06cb362918
Alexey_js <alexey.grishchenko@mail.ru>
2020-12-19 20:19:37 +0300

    Reworked server and client code in object oriented programming style

--- tim-client.py
+++ tim-client.py
@@ -3,36 +3,34 @@ import threading
 import time
 
 
+class Client:
+    def __init__(self):
 
-PORT = 5050
-FORMAT = "utf-8"
-HEADER = 64
-DISCONNECT_MESSAGE = "!Disconnect"
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER,PORT)
+        self.SERVERPORT = 5050
+        self.FORMAT = "utf-8"
+        self.HEADERSIZE = 64
+        self.IP = socket.gethostbyname(socket.gethostname())
+        self.SERVERADDR = (self.IP, self.SERVERPORT)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(ADDR)
+    def start(self):
+        self.sock.connect(self.SERVERADDR)
+
+    def send(self, msg):  # variables in argument may change
+        FORMAT = self.FORMAT
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' '*(self.HEADERSIZE - len(send_length))
+        self.sock.send(send_length)
+        self.sock.send(message)
+        response = self.sock.recv(2048)
+        print(response)
+# objective: send commands to server in json format
 
 
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' '*(HEADER  - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    response = client.recv(2048)
-    print(response)
-# objective: send commands to server in json format 
-
-
-send("-s: Hello world")
+client = Client()
+client.start()
+client.send("-s: Hello world")
 time.sleep(2)
-send("-ex:")
-# trial : -s message  server prints message on its side
-# -ex whatever I write : stop server
-
-
-    
+client.send("-ex:")
--- tim-server.py
+++ tim-server.py
@@ -4,65 +4,72 @@ import time
 import sys
 import os
 
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-TERMINATED = False
-ADDR = (SERVER,PORT)
-HEADER = 64 # each message contains this number of bytes coming before the message
-# this info contains number bytesize of the main message
-FORMAT = "utf-8"
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(ADDR)
+# PORT = 5050
+# SERVER = socket.gethostbyname(socket.gethostname())
+# TERMINATED = False
+# ADDR = (SERVER,PORT)
+# HEADERSIZE = 64 # each message contains this number of bytes coming before the message
+# # this info contains number bytesize of the main message
+# FORMAT = "utf-8"
+# server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# server.bind(ADDR)
 
-def exec_exit():
-    print("Shutting the server down")
-    os._exit(0)
-    
-def parse_cmd(msg):
-    cmd = msg[0:3]
-    if cmd=="-ex":
-        print("EXIT command received")
-        return "EXIT"
-    else:
-        print("unknown command received")
-        return None
-        
-    
-    
 
-def handle_client(conn,addr):
-    global TERMINATED
-    print(f"[New connection] {addr} connected")
-    to_exit = False
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            cmd = parse_cmd(msg)
-            if cmd == "EXIT":
-                connected = False
-                to_exit = True
-            
-            print(f"[{addr}] {msg}")
-            conn.send(b"Msg received")
-    
-    conn.close()
-    if to_exit:
-        exec_exit()
-        
-def start(): #  handles all connection and passes each connection for handle_client
-    server.listen()
-    print(f"[Listening] Server is listening on {SERVER}")
-    while True:
-        conn,addr = server.accept()
-        thread = threading.Thread(target = handle_client,args = (conn,addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
-        
-        
-        
+class Server:
+    def __init__(self):
+        self.HEADERSIZE = 64
+        self.PORT = 5050
+        self.IP = socket.gethostbyname(socket.gethostname())
+        self.ADDR = (self.IP, self.PORT)
+        self.FORMAT = "utf-8"
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    def exec_exit(self):
+        print("Shutting the server down")
+        os._exit(0)
+
+    def parse_cmd(self, msg):
+        cmd = msg[0:3]
+        if cmd == "-ex":
+            print("EXIT command received")
+            return "EXIT"
+        else:
+            print("unknown command received")
+            return None
+
+    def handle_client(self, conn, addr):
+        print(f"[New connection] {addr} connected")
+        to_exit = False
+        connected = True
+        while connected:
+            msg_length = conn.recv(self.HEADERSIZE).decode(self.FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(self.FORMAT)
+                cmd = self.parse_cmd(msg)
+                if cmd == "EXIT":
+                    connected = False
+                    to_exit = True
+
+                print(f"[{addr}] {msg}")
+                conn.send(b"Msg received")
+
+        conn.close()
+        if to_exit:
+            self.exec_exit()
+
+    def start(self):  # handles all connection and passes each connection for handle_client
+        self.sock.bind(self.ADDR)
+        self.sock.listen()
+        print(f"[Listening] Server is listening on {self.IP}")
+        while True:
+            conn, addr = self.sock.accept()
+            thread = threading.Thread(
+                target=self.handle_client, args=(conn, addr))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
+
+
+server = Server()
 print("Starting server")
-start()
+server.start()
